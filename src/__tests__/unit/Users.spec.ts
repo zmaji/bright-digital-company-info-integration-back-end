f8fa8f8878b178c27ea4c232e807cb9ddec602cc
Maurice ten Teije <mauricetenteije@MacBook-Pro-van-Maurice.local>
2024-04-11 15:40:26 +0200

    Updated usertests

--- src/__tests__/unit/Users.spec.ts
+++ src/__tests__/unit/Users.spec.ts
@@ -5,82 +5,81 @@ import { v4 as uuidv4 } from 'uuid';
 
 describe('Users Controller Tests', () => {
   let hashedPassword = '';
-  let secret = '';
+  const password = '123'
   const emailAddress = 'maurice@brightdigital.com';
-  const portalId = 123;
-  const accessToken = '123';
-  const refreshToken = '123';
   const userId = 1;
+  const secret = uuidv4();
+  const activationToken = uuidv4();
+
+  const data = {
+    firstName: 'Maurice',
+    lastName: 'ten Teije',
+    emailAddress: emailAddress,
+    password: hashedPassword,
+    secret: secret,
+    roles: ['Gebruiker'],
+    isActive: false,
+    activationToken: activationToken,
+  };
 
   beforeAll(async () => {
-    hashedPassword = await bcrypt.hash('123', 12);
-    secret = uuidv4();
+    hashedPassword = await bcrypt.hash(password, 12);
   });
 
   test('should create a new system user', async () => {
-    const data = {
-      emailAddress: emailAddress,
-      password: hashedPassword,
-      secret: secret,
-      roles: ['Gebruiker'],
-    };
-
     prismaMock.user.create.mockResolvedValueOnce({
-      id: userId,
-      hubSpotPortalId: null,
-      ...data,
-    });
-
+        id: userId,
+        hubSpotPortalId: null,
+        domain: null,
+        companyInfoUserName: null,
+        companyInfoPassword: null,
+        ...data,
+      });
+      
     await expect(usersController.createUser(data)).resolves.toEqual({
       id: userId,
       hubSpotPortalId: null,
-      emailAddress: emailAddress,
-      password: hashedPassword,
-      secret: secret,
-      roles: ['Gebruiker'],
+      domain: null,
+      companyInfoUserName: null,
+      companyInfoPassword: null,
+      ...data,
     });
   });
 
   test('should handle error during user creation', async () => {
-    const data = {
-      emailAddress: emailAddress,
-      password: hashedPassword,
-      secret: secret,
-      roles: ['Gebruiker'],
-    };
-
     prismaMock.user.create.mockRejectedValueOnce(new Error('Failed to create user'));
 
-    await expect(usersController.createUser(data)).rejects.toThrowError('Failed to create user');
+    await expect(usersController.createUser(data)).rejects.toThrow('Failed to create user');
   });
 
   test('should get user by email address', async () => {
     prismaMock.user.findUnique.mockResolvedValueOnce({
-      id: 1,
-      hubSpotPortalId: null,
-      emailAddress: emailAddress,
-      password: hashedPassword,
-      secret: secret,
-      roles: ['Gebruiker'],
-    });
-
+        id: userId,
+        hubSpotPortalId: null,
+        domain: null,
+        companyInfoUserName: null,
+        companyInfoPassword: null,
+        ...data,
+      });
+  
     await expect(usersController.getUser(emailAddress)).resolves.toEqual({
-      id: 1,
-      hubSpotPortalId: null,
-      emailAddress: emailAddress,
-      password: hashedPassword,
-      secret: secret,
-      roles: ['Gebruiker'],
+        id: userId,
+        hubSpotPortalId: null,
+        domain: null,
+        companyInfoUserName: null,
+        companyInfoPassword: null,
+        ...data,
     });
   });
+  
 
   test('should handle error during user retrieval', async () => {
     prismaMock.user.findUnique.mockRejectedValueOnce(new Error('Failed to find user'));
 
-    await expect(usersController.getUser(emailAddress)).rejects.toThrowError('Failed to find user');
+    await expect(usersController.getUser(emailAddress)).rejects.toThrow('Failed to find user');
   });
 
-  test('should return null if user not found by email address', async () => {
+  test('should handle null if user not found by email address', async () => {
     const nonExistingEmail = 'nonexistent@brightdigital.com';
 
     prismaMock.user.findUnique.mockResolvedValueOnce(null);
@@ -88,60 +87,34 @@ describe('Users Controller Tests', () => {
     await expect(usersController.getUser(nonExistingEmail)).resolves.toBeNull();
   });
 
-  test('should update user with hubSpotPortalId when token exists', async () => {
-    prismaMock.hubToken.findUnique.mockResolvedValueOnce({
-      id: 1,
-      portal_id: portalId,
-      access_token: accessToken,
-      refresh_token: refreshToken,
-      expires_in: 3600,
-      created_at: new Date(),
-      updated_at: new Date(),
-    });
-
-    prismaMock.user.update.mockResolvedValueOnce({
+  test('should update user with specified fields', async () => {
+    const updateFields = {
+      firstName: 'NewFirstName',
+    };
+  
+    const updatedUserMock = {
       id: userId,
-      hubSpotPortalId: portalId,
-      emailAddress: emailAddress,
-      password: hashedPassword,
-      secret: secret,
-      roles: ['Gebruiker'],
-    });
-
-    const result = await usersController.updateUser(accessToken, userId);
-
-    expect(result).toEqual({
+      ...updateFields,
+    };
+  
+    // @ts-ignore
+    prismaMock.user.update.mockResolvedValueOnce(updatedUserMock); 
+  
+    const expectedResponse = {
       id: userId,
-      hubSpotPortalId: portalId,
-      emailAddress: emailAddress,
-      password: hashedPassword,
-      secret: secret,
-      roles: ['Gebruiker'],
-    });
+      ...updateFields,
+    };
+  
+    await expect(usersController.updateUser(userId, updateFields)).resolves.toEqual(expectedResponse);
   });
-
-  test('should return null if access token not found', async () => {
-    const accessToken = 'nonexistentAccessToken';
-    const userId = 1;
-
-    prismaMock.hubToken.findUnique.mockResolvedValueOnce(null);
-
-    await expect(usersController.updateUser(accessToken, userId)).resolves.toBeNull();
-  });
-
+  
   test('should handle error during user update', async () => {
-    prismaMock.hubToken.findUnique.mockResolvedValueOnce({
-      id: 1,
-      portal_id: portalId,
-      access_token: accessToken,
-      refresh_token: refreshToken,
-      expires_in: 3600,
-      created_at: new Date(),
-      updated_at: new Date(),
-    });
+    const updateFields = {
+      firstName: 'NewFirstName',
+    };
 
     prismaMock.user.update.mockRejectedValueOnce(new Error('Failed to update user'));
 
-    await expect(usersController.updateUser(accessToken, userId)).rejects.toThrowError('Failed to update user');
+    await expect(usersController.updateUser(userId, updateFields)).rejects.toThrowError('Failed to update user');
   });
 });
