2f231d0b2249feec49030025b12a4c20af056fe3
Maurice ten Teije <mauricetenteije@MacBook-Pro-van-Maurice.local>
2024-04-12 14:24:44 +0200

    Complete webhooks tests

--- src/__tests__/unit/Webhooks.spec.ts
+++ src/__tests__/unit/Webhooks.spec.ts
@@ -6,13 +6,25 @@ jest.mock('axios');
 jest.mock('../../utils/Logger');
 
 describe('webHookController Tests', () => {
+    const period = 'weekly';
+    const maxConcurrentRequests = 20;
+    const targetUrl = 'https://www.example.com/hubspot/target';
+
+    const webhook = {
+        throttling: {
+          period: period,
+          maxConcurrentRequests: maxConcurrentRequests,
+        },
+        targetUrl: `${targetUrl}/webhooks/company`,
+      };
+
     const webhookReponse = {
         createdAt: "2024-04-12T11:30:09.527Z",
         throttling: {
-          period: "SECONDLY",
-          maxConcurrentRequests: 0
+          period: "weekly",
+          maxConcurrentRequests: 20
         },
-        targetUrl: "https://www.example.com/hubspot/target",
+        targetUrl: "https://www.example.com/hubspot/target/webhooks/company",
         updatedAt: "2024-04-12T11:30:09.527Z"
       };
   
@@ -55,63 +67,37 @@ describe('webHookController Tests', () => {
     });
 
     test('should create webhook successfully', async () => {
-      const period = 'daily';
-      const maxConcurrentRequests = 10;
-      const targetUrl = 'https://example.com';
-
-      const requestData = {
-        throttling: {
-          period: period,
-          maxConcurrentRequests: maxConcurrentRequests,
-        },
-        targetUrl: `${targetUrl}/webhooks/company`,
-      };
-
       // @ts-ignore
-      axios.post.mockResolvedValue({ data: requestData });
+      axios.post.mockResolvedValue({ data: webhook });
 
       const result = await webHookController.createWebHook(period, maxConcurrentRequests, targetUrl);
 
-      expect(axios.post).toHaveBeenCalledWith(`https://api.hubapi.com/webhooks/v3/${process.env.HUBSPOT_APP_ID}/settings`, requestData, {
+      expect(axios.post).toHaveBeenCalledWith(`https://api.hubapi.com/webhooks/v3/${process.env.HUBSPOT_APP_ID}/settings`, webhook, {
         params: { hapikey: process.env.HUBSPOT_APP_DEVELOPER_KEY },
         headers: { 'Content-Type': 'application/json' },
       });
       expect(logger.info).toHaveBeenCalledWith('Initializing a HubSpot Webhook..');
-      expect(logger.info).toHaveBeenCalledWith(JSON.stringify(requestData));
+      expect(logger.info).toHaveBeenCalledWith(JSON.stringify(webhook));
       expect(logger.info).toHaveBeenCalledWith('Webhook created successfully');
-      expect(result).toEqual(webhookReponse);
+      expect(result).toEqual(webhook);
     });
 
     test('should update webhook successfully', async () => {
-      const period = 'weekly';
-      const maxConcurrentRequests = 20;
-      const targetUrl = 'https://example.com';
-
-      const requestData = {
-        throttling: {
-          period: period,
-          maxConcurrentRequests: maxConcurrentRequests,
-        },
-        targetUrl: `${targetUrl}/webhooks/company`,
-      };
-
-      const responseData = { webhook: 'updated' };
-
       // @ts-ignore
-      axios.put.mockResolvedValue({ data: responseData });
+      axios.put.mockResolvedValue({ data: webhook });
 
       const result = await webHookController.updateWebHook(period, maxConcurrentRequests, targetUrl);
 
-      expect(axios.put).toHaveBeenCalledWith(`https://api.hubapi.com/webhooks/v3/${process.env.HUBSPOT_APP_ID}/settings`, requestData, {
+      expect(axios.put).toHaveBeenCalledWith(`https://api.hubapi.com/webhooks/v3/${process.env.HUBSPOT_APP_ID}/settings`, webhook, {
         params: { hapikey: process.env.HUBSPOT_APP_DEVELOPER_KEY },
         headers: { 'Content-Type': 'application/json' },
       });
       expect(logger.info).toHaveBeenCalledWith('Updating a HubSpot Webhook..');
-      expect(logger.info).toHaveBeenCalledWith(JSON.stringify(responseData));
+      expect(logger.info).toHaveBeenCalledWith(JSON.stringify(webhook));
       expect(logger.info).toHaveBeenCalledWith('Webhook updated successfully');
       expect(logger.info).toHaveBeenCalledWith('Webhook target URL changed to:', `${targetUrl}/webhooks/company`);
       expect(logger.info).toHaveBeenCalledWith(`${targetUrl}/webhooks/company`);
-      expect(result).toEqual(responseData);
+      expect(result).toEqual(webhook);
     });
 
     test('should retrieve webhook subscriptions successfully', async () => {
