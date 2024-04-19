aab68afc31c8c108f5c2de511d72358118c11217
Maurice ten Teije <mauricetenteije@MacBook-Pro-van-Maurice.local>
2024-04-19 10:56:38 +0200

    WIP: Company detail; stagnated because credits exploited

--- src/controllers/Companies.ts
+++ src/controllers/Companies.ts
@@ -44,6 +44,8 @@ const getCompanies = async (tradeName: string): Promise<Company[] | null> => {
 };
 
 const getCompanyInfo = async (dossierNumber: number): Promise<CompanyDetail | null> => {
+  logger.info('Trying to get company info');
+  
   try {
     const client = await soap.createClientAsync(companyInfoURL);
     const soapHeader = {
