3fc6b0188d54d18dfda3f2c7165d7b8846db8091
Maurice <502241@student.saxion.nl>
2024-04-23 07:45:17 +0200

    sparkpost off till access to account

--- src/helpers/sendActivationEmail.ts
+++ src/helpers/sendActivationEmail.ts
@@ -1,26 +1,26 @@
-import logger from '../utils/Logger';
-import SparkPost from 'sparkpost';
-import dotenv from 'dotenv';
+// import logger from '../utils/Logger';
+// import SparkPost from 'sparkpost';
+// import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
-const SPARKPOST_API_KEY = process.env.SPARKPOST_API_KEY;
-const sparkpostClient = new SparkPost(SPARKPOST_API_KEY, { origin: "https://api.eu.sparkpost.com:443" });
+// const SPARKPOST_API_KEY = process.env.SPARKPOST_API_KEY;
+// const sparkpostClient = new SparkPost(SPARKPOST_API_KEY, { origin: "https://api.eu.sparkpost.com:443" });
 
-export const sendActivationEmail = async (email: string, activationToken: string) => {
-  logger.info(`Sending activation email to ${email}`);
+// export const sendActivationEmail = async (email: string, activationToken: string) => {
+//   logger.info(`Sending activation email to ${email}`);
 
-  try {
-    await sparkpostClient.transmissions.send({
-      content: {
-        from: 'noreply@brightdigital.dev',
-        subject: 'Activate Your Account',
-        html: `<p>Your activation code is: ${activationToken}</p>`,
-      },
-      recipients: [{ address: email }],
-    });
-  } catch (error) {
-    logger.error('Error sending email verification:', error);
-    throw error;
-  }
-};
+//   try {
+//     await sparkpostClient.transmissions.send({
+//       content: {
+//         from: 'noreply@brightdigital.dev',
+//         subject: 'Activate Your Account',
+//         html: `<p>Your activation code is: ${activationToken}</p>`,
+//       },
+//       recipients: [{ address: email }],
+//     });
+//   } catch (error) {
+//     logger.error('Error sending email verification:', error);
+//     throw error;
+//   }
+// };
