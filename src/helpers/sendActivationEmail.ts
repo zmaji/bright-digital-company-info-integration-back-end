3ecaf49d0074c8482c960750049d1b4cada77050
Maurice ten Teije <mauricetenteije@MacBook-Pro-van-Maurice.local>
2024-05-16 16:16:52 +0200

    Email template styling

--- src/helpers/sendActivationEmail.ts
+++ src/helpers/sendActivationEmail.ts
@@ -28,15 +28,17 @@ const emailTemplate = (firstName: string, lastName:string, activationToken: stri
 
         .v-template__squeeze {
             max-width: 600px;
+            width: 100%;
             margin: 0 auto;
+            background-color: #ffffff;
         }
 
         .v-template__header-container {
             background-color: #212121;
             height: 65px;
             padding: 8px;
-            display: table;
             width: 100%;
+            box-sizing: border-box;
         }
 
         .v-template__header-logo {
@@ -70,7 +72,6 @@ const emailTemplate = (firstName: string, lastName:string, activationToken: stri
 
         .v-template__content-container {
             padding: 25px 50px 50px 50px;
-            background-color: #fff;
         }
 
         .v-template__content-paragraph {
@@ -161,72 +162,74 @@ const emailTemplate = (firstName: string, lastName:string, activationToken: stri
     </style>
 </head>
 <body class="v-template">
-    <table class="v-template__container" cellpadding="0" cellspacing="0">
-        <tr>
-            <td class="v-template__squeeze">
-                <table class="v-template__header-container">
-                    <tr>
-                        <td><img class="v-template__header-logo" src="https://cdn2.hubspot.net/hub/248909/hubfs/Bright_logo/Bright-logo-wit.png?width=240&upscale=true&name=Bright-logo-wit.png" alt="Bright Digital Logo" /></td>
-                        <td class="v-template__header-title">Company.info Integration</td>
-                    </tr>
-                </table>
+    <div class="v-template__container">
+        <table class="v-template__squeeze" cellpadding="0" cellspacing="0">
+            <tr>
+                <td>
+                    <table class="v-template__header-container">
+                        <tr>
+                            <td><img class="v-template__header-logo" src="https://cdn2.hubspot.net/hub/248909/hubfs/Bright_logo/Bright-logo-wit.png?width=240&upscale=true&name=Bright-logo-wit.png" alt="Bright Digital Logo" /></td>
+                            <td class="v-template__header-title">Company.info Integration</td>
+                        </tr>
+                    </table>
 
-                <div class="v-template__seperator"></div>
+                    <div class="v-template__seperator"></div>
 
-                <div class="v-template__image-container"></div>
+                    <div class="v-template__image-container"></div>
 
-                <div class="v-template__content-container">
-                    <h1 class="v-template__content-title">Hi ${firstName} ${lastName},</h1>
+                    <div class="v-template__content-container">
+                        <h1 class="v-template__content-title">Hi ${firstName} ${lastName},</h1>
 
-                    <div class="v-template__content-paragraph">
-                        Thank you for registering. Please enter the following code to activate your account:
-                        <div class="v-template__content-code">${activationToken}</div>
+                        <div class="v-template__content-paragraph">
+                            Thank you for registering. Please enter the following code to activate your account:
+                            <div class="v-template__content-code">${activationToken}</div>
+                        </div>
                     </div>
-                </div>
-            </td>
-        </tr>
-        <tr>
-            <td class="v-template__footer">
-                <img class="v-template__footer-logo" src="https://cdn2.hubspot.net/hub/248909/hubfs/Bright_logo/Bright-logo-wit.png?width=240&upscale=true&name=Bright-logo-wit.png" alt="Bright Digital Logo" />
+                </td>
+            </tr>
+            <tr>
+                <td class="v-template__footer">
+                    <img class="v-template__footer-logo" src="https://cdn2.hubspot.net/hub/248909/hubfs/Bright_logo/Bright-logo-wit.png?width=240&upscale=true&name=Bright-logo-wit.png" alt="Bright Digital Logo" />
 
-                <div class="v-template__footer-paragraph">The full-service partner for companies that want to grow smarter with HubSpot</div>
+                    <div class="v-template__footer-paragraph">The full-service partner for companies that want to grow smarter with HubSpot</div>
 
-                <div class="v-template__socials-container">
-                    <a class="v-template__social-ref" href="https://www.linkedin.com/company/2632117" target="_blank" rel="noopener noreferrer">
-                        <img class="v-template__socials-item" src="https://www.brightdigital.com/hs/hsstatic/TemplateAssets/static-1.262/img/hs_default_template_images/modules/Follow+Me+-+Email/linkedin_original_white.png" alt="LinkedIn logo" />
-                    </a>
+                    <div class="v-template__socials-container">
+                        <a class="v-template__social-ref" href="https://www.linkedin.com/company/2632117" target="_blank" rel="noopener noreferrer">
+                            <img class="v-template__socials-item" src="https://www.brightdigital.com/hs/hsstatic/TemplateAssets/static-1.262/img/hs_default_template_images/modules/Follow+Me+-+Email/linkedin_original_white.png" alt="LinkedIn logo" />
+                        </a>
 
-                    <a class="v-template__social-ref" href="https://twitter.com/BureauBright" target="_blank" rel="noopener noreferrer">
-                        <img class="v-template__socials-item" src="https://www.brightdigital.com/hs/hsstatic/TemplateAssets/static-1.262/img/hs_default_template_images/modules/Follow+Me+-+Email/twitter_original_white.png" alt="X logo" />
-                    </a>
+                        <a class="v-template__social-ref" href="https://twitter.com/BureauBright" target="_blank" rel="noopener noreferrer">
+                            <img class="v-template__socials-item" src="https://www.brightdigital.com/hs/hsstatic/TemplateAssets/static-1.262/img/hs_default_template_images/modules/Follow+Me+-+Email/twitter_original_white.png" alt="X logo" />
+                        </a>
 
-                    <a class="v-template__social-ref" href="https://www.instagram.com/brightwaytogrow/" target="_blank" rel="noopener noreferrer">
-                        <img class="v-template__socials-item" src="https://www.brightdigital.com/hs/hsstatic/TemplateAssets/static-1.262/img/hs_default_template_images/modules/Follow+Me+-+Email/instagram_original_white.png" alt="Instagram logo" />
-                    </a>
+                        <a class="v-template__social-ref" href="https://www.instagram.com/brightwaytogrow/" target="_blank" rel="noopener noreferrer">
+                            <img class="v-template__socials-item" src="https://www.brightdigital.com/hs/hsstatic/TemplateAssets/static-1.262/img/hs_default_template_images/modules/Follow+Me+-+Email/instagram_original_white.png" alt="Instagram logo" />
+                        </a>
 
-                    <a class="v-template__social-ref" href="https://www.facebook.com/Brightwaytogrow/" target="_blank" rel="noopener noreferrer">
-                        <img class="v-template__socials-item" src="https://www.brightdigital.com/hs/hsstatic/TemplateAssets/static-1.262/img/hs_default_template_images/modules/Follow+Me+-+Email/facebook_original_white.png" alt="Facebook logo" />
-                    </a>
-                </div>
+                        <a class="v-template__social-ref" href="https://www.facebook.com/Brightwaytogrow/" target="_blank" rel="noopener noreferrer">
+                            <img class="v-template__socials-item" src="https://www.brightdigital.com/hs/hsstatic/TemplateAssets/static-1.262/img/hs_default_template_images/modules/Follow+Me+-+Email/facebook_original_white.png" alt="Facebook logo" />
+                        </a>
+                    </div>
 
-                <div class="v-template__seperator-line"></div>
+                    <div class="v-template__seperator-line"></div>
 
-                <div class="v-template__links-container">
-                    <a class="v-template__links-item" href="https://www.brightdigital.com/nl/" target="_blank" rel="noopener noreferrer">Brightdigital.com</a>
+                    <div class="v-template__links-container">
+                        <a class="v-template__links-item" href="https://www.brightdigital.com/nl/" target="_blank" rel="noopener noreferrer">Brightdigital.com</a>
 
-                    <span class="v-template__word-seperator">|</span>
+                        <span class="v-template__word-seperator">|</span>
 
-                    <a class="v-template__links-item" href="https://www.brightdigital.com/nl/privacyverklaring" target="_blank" rel="noopener noreferrer">Privacyverklaring</a>
+                        <a class="v-template__links-item" href="https://www.brightdigital.com/nl/privacyverklaring" target="_blank" rel="noopener noreferrer">Privacyverklaring</a>
 
-                    <span class="v-template__word-seperator">|</span>
+                        <span class="v-template__word-seperator">|</span>
 
-                    <a class="v-template__links-item" href="https://www.brightdigital.com/hubfs/Bright%20Digital%20website/2022%20_%20Algemene%20voorwaarden%20_%20Bright%20Digital%20B.V..pdf" target="_blank" rel="noopener noreferrer">Algemene voorwaarden</a>
-                </div>
+                        <a class="v-template__links-item" href="https://www.brightdigital.com/hubfs/Bright%20Digital%20website/2022%20_%20Algemene%20voorwaarden%20_%20Bright%20Digital%20B.V..pdf" target="_blank" rel="noopener noreferrer">Algemene voorwaarden</a>
+                    </div>
 
-                <div class="v-template__footer-address">Bright Digital B.V., Vosselmanstraat 300, Apeldoorn, Gelderland, +31 (0)85 760 81 81</div>
-            </td>
-        </tr>
-    </table>
+                    <div class="v-template__footer-address">Bright Digital B.V., Vosselmanstraat 300, Apeldoorn, Gelderland, +31 (0)85 760 81 81</div>
+                </td>
+            </tr>
+        </table>
+    </div>
 </body>
 </html>
 `;
