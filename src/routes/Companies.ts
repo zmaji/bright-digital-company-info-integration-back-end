c209073e8331de85cd0053612b1e505e4927dfe0
Maurice ten Teije <mauricetenteije@MacBook-Pro-van-Maurice.local>
2024-05-23 13:42:21 +0200

    Changed form webhook

--- src/routes/Companies.ts
+++ src/routes/Companies.ts
@@ -14,7 +14,7 @@ const router = Router();
 const url = 'https://ws1.webservices.nl/soap_doclit?wsdl'
 const args = { username: process.env.COMPANY_INFO_TEST_USERNAME, password: process.env.COMPANY_INFO_TEST_PASSWORD }
 
-router.get('webhook', isLoggedIn, async (req: Request, res: Response) => {
+router.get('/webhook', isLoggedIn, async (req: Request, res: Response) => {
   console.log('Entered form webhook');
 
   soap.createClient(url, async (err, client) => {
