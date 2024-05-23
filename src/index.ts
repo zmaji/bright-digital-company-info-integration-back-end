76500395b2505484a07597ffd41921e1c715626c
Maurice ten Teije <mauricetenteije@MacBook-Pro-van-Maurice.local>
2024-05-23 12:51:53 +0200

    Removed intializing webhook on acceptance stage

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import type { Express } from 'express';
 
 import logger from '../src/utils/Logger';
 import createServer from './utils/Server';
-import { initializeWebhook } from './utils/Webhook';
+// import { initializeWebhook } from './utils/Webhook';
 
 const startServer = async () => {
   const app: Express = createServer();
   const port = process.env.PORT || 3000;
 
-  await initializeWebhook();
+  // await initializeWebhook();
 
   app.listen(port, () => {
     logger.info(`Company.info integration back-end listening on port ${port}`);
