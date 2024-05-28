b241a742eff99d1404d39aae29527042b4c6fbff
Maurice <502241@student.saxion.nl>
2024-05-28 14:00:28 +0200

    WIP CRM webhook set-up

--- src/routes/Webhooks.ts
+++ src/routes/Webhooks.ts
@@ -108,6 +108,22 @@ router.get('/datarequest', async (req: Request, res: Response) => {
     const dossierNumber = req.query.dossier_number;
     const tradeName = req.query.name;
     const objectId = req.query.associatedObjectId;
+    let status: string;s
+    let statusType: string;
+    let buttonLabel: string;
+    let confirmationMessage: string;
+
+    if (dossierNumber !== '' && dossierNumber !== undefined && dossierNumber !== null) {
+      status = 'Synced';
+      statusType = 'SUCCESS';
+      buttonLabel = 'Sync with Company.info';
+      confirmationMessage = `Are you sure you want to sync ${tradeName}?`
+    } else {
+      status = 'Not synced';
+      statusType = 'DANGER';
+      buttonLabel = 'Update company';
+      confirmationMessage = `Are you sure you want to update ${tradeName}?`
+    }
 
     const cardInformation = {
       'results': [
@@ -120,20 +136,20 @@ router.get('/datarequest', async (req: Request, res: Response) => {
           'updated': '2016-09-28',
           'properties': [
             {
-              'label': 'Bedrijfsnaam',
+              'label': 'Trade name',
               'dataType': 'STRING',
               'value': tradeName,
             },
             {
-              'label': 'Status',
-              'dataType': 'STATUS',
-              'value': 'TEST2!!!!!',
-              "optionType": "DANGER",
+              'label': 'Dossier number',
+              'dataType': 'NUMERIC',
+              'value': dossierNumber,
             },
             {
-              'label': '121221',
-              'dataType': 'STRING',
-              'value': 'TEST3',
+              'label': 'Company.info status',
+              'dataType': 'STATUS',
+              'value': status,
+              'optionType': statusType,
             },
           ],
         },
@@ -167,5 +183,94 @@ router.get('/datarequest', async (req: Request, res: Response) => {
         .json({ error: 'An error occurred processing the webhook' });
   }
 });
+
+// router.get('/datarequest', async (req: Request, res: Response) => {
+//   logger.info('Entered datarequest webhook route!');
+//   try {
+//     // const verified = await basicVerification(req);
+
+// // DEFAULT: Grey
+// // SUCCESS: Green
+// // WARNING: Yellow
+// // DANGER: Red
+// // INFO: Blue
+
+// // CURRENCY, DATE, DATETIME, EMAIL, LINK, NUMERIC, STATUS, STRING
+
+//     // if (verified) {
+//     // const portalId = req.query.portalId;
+//     const dossierNumber = req.query.dossier_number;
+//     const tradeName = req.query.name;
+//     let status: string;
+//     let statusType: string;
+//     let buttonLabel: string;
+//     let confirmationMessage: string;
+
+//     if (dossierNumber !== '' && dossierNumber !== undefined && dossierNumber !== null) {
+//       status = 'Synced';
+//       statusType = 'SUCCESS';
+//       buttonLabel = 'Sync with Company.info';
+//       confirmationMessage = `Are you sure you want to sync ${tradeName}?`
+//     } else {
+//       status = 'Not synced';
+//       statusType = 'DANGER';
+//       buttonLabel = 'Update company';
+//       confirmationMessage = `Are you sure you want to update ${tradeName}?`
+//     }
+
+//     const cardInformation = {
+//       'results': [
+//         {
+//           'properties': [
+//             {
+//               'label': 'Trade name',
+//               'dataType': 'STRING',
+//               'value': tradeName,
+//             },
+//             {
+//               'label': 'Dossier number',
+//               'dataType': 'NUMERIC',
+//               'value': dossierNumber,
+//             },
+//             {
+//               'label': 'Company.info status',
+//               'dataType': 'STATUS',
+//               'value': status,
+//               'optionType': statusType,
+//             },
+//           ],
+//         },
+//       ],
+//       'primaryAction': {
+//         "type": "CONFIRMATION_ACTION_HOOK",
+//         "httpMethod": "POST",
+//         "uri": "https://example.com/action-hook",
+//         "label": buttonLabel,
+//         "associatedObjectProperties": [
+//           "some_crm_property"
+//         ],
+//         "confirmationMessage": buttonLabel,
+//         "confirmButtonText": "Yes",
+//         "cancelButtonText": "No"
+//       },
+//       'secondaryActions':[
+//         {
+//         'type': 'IFRAME',
+//         'width': 890,
+//         'height': 748,
+//         'uri': 'https://example.com/settings-iframe-contents',
+//         'label': 'Settings',
+//         }
+//       ]   
+//     };
+
+//     res.send(cardInformation);
+//     // }
+//   } catch (error) {
+//     res
+//         .status(StatusCodes.INTERNAL_SERVER_ERROR)
+//         .json({ error: 'An error occurred processing the webhook' });
+//   }
+// });
 
 export default router;
