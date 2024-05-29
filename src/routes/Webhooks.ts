2de9d097207cdc11c0af896cbca15f5e1bb6c9e7
Maurice <502241@student.saxion.nl>
2024-05-29 10:22:55 +0200

    iframe styling WIP

--- src/routes/Webhooks.ts
+++ src/routes/Webhooks.ts
@@ -104,8 +104,12 @@ router.put('/update', async (req: Request, res: Response) => {
         const companyId = req.body.companyId as string;
         const companyData = req.body.companyData;
 
+        console.log('companyId')
+        console.log(companyId)
+        console.log('companyData')
+        console.log(companyData)
+
         if (hubToken && companyId && companyData) {
-          console.log('hi');
 
           if (companyId && companyId !== '' && companyData && Object.keys(companyData).length > 0) {
             const result = await companiesController.updateCompany(hubToken, companyId, companyData);
