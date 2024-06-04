d008c9c28ecc5ff7c22b57eb4a207e8b75184920
Maurice <502241@student.saxion.nl>
2024-06-04 14:42:48 +0200

    WIP: Show last_sync on CRM card

--- src/routes/Webhooks.ts
+++ src/routes/Webhooks.ts
@@ -29,7 +29,7 @@ const formatDateFromTimestamp = (timestamp: string) => {
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
   const day = String(date.getDate()).padStart(2, '0');
-  return `${year}-${month}-${day}`;
+  return `${day}-${month}-${year}`;
 };
 
 router.post('/company', async (req: Request, res: Response) => {
