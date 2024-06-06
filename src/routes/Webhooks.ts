662b49478a40533850edaa04aa52e046f90b2f47
Maurice ten Teije <mauricetenteije@MacBook-Pro-van-Maurice.local>
2024-06-06 14:38:01 +0200

    WIP: double webhooks

--- src/routes/Webhooks.ts
+++ src/routes/Webhooks.ts
@@ -22,8 +22,10 @@ const formatDate = (date) => {
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
-
-  return `${year}-${month}-${day}`;
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
 };
 
 const formatDateFromTimestamp = (timestamp: string) => {
