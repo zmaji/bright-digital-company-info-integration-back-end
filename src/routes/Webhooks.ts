c75d85ee9261d5a425a89df081ae0e64935fc398
Maurice ten Teije <mauricetenteije@MacBook-Pro-van-Maurice.local>
2024-06-06 15:38:17 +0200

    WIP: double webhooks

--- src/routes/Webhooks.ts
+++ src/routes/Webhooks.ts
@@ -54,6 +54,11 @@ const parseDate = (dateString: string) => {
 const isLessThan10SecondsAgo = (lastSyncString: string) => {
   const lastSyncDate = parseDate(lastSyncString);
   const currentDate = new Date();
+  
+  console.log('lastSyncDate.getTime()')
+  console.log(lastSyncDate.getTime())
+  console.log('currentDate.getTime()')
+  console.log(currentDate.getTime())
 
   const timeDifferenceInSeconds = (currentDate.getTime() - lastSyncDate.getTime()) / 1000;
 
