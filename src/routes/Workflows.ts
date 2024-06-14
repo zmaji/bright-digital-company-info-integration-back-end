35318cc0b49d7195a2601ac462b7897920611fec
Maurice ten Teije <mauricetenteije@MacBook-Pro-van-Maurice.local>
2024-06-14 10:54:10 +0200

    WIP: Custom workflow action

--- src/routes/Workflows.ts
+++ src/routes/Workflows.ts
@@ -4,6 +4,13 @@ import dotenv from 'dotenv';
 import axios from 'axios';
 import { StatusCodes } from 'http-status-codes';
 import { verifySignature } from '../helpers/hubspot/verifySignature';
+import { CompanyDetail } from '../typings/CompanyDetail';
+import companiesController from '../controllers/Companies';
+import { User } from '../typings/User';
+import usersController from '../controllers/Users';
+import { formatCompanyData } from '../helpers/hubspot/formatCompanyData';
+import authController from '../controllers/Auth';
+import { HubToken } from '../typings/HubToken';
 
 dotenv.config();
 const router = Router();
@@ -11,12 +18,37 @@ const router = Router();
 const HUBSPOT_APP_ID = process.env.HUBSPOT_APP_ID;
 const HUBSPOT_APP_DEVELOPER_KEY = process.env.HUBSPOT_APP_DEVELOPER_KEY;
 
-router.post('/definition', async (req: Request, res: Response) => {
+const formatDate = (date: Date) => {
+    const options = {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false,
+      timeZone: 'Europe/Amsterdam',
+    };
+  
+    // @ts-expect-error options not part of..
+    const formattedDateParts = new Intl.DateTimeFormat('en-GB', options).formatToParts(date);
+  
+    const day = formattedDateParts.find((part) => part.type === 'day').value;
+    const month = formattedDateParts.find((part) => part.type === 'month').value;
+    const year = formattedDateParts.find((part) => part.type === 'year').value;
+    const hours = formattedDateParts.find((part) => part.type === 'hour').value;
+    const minutes = formattedDateParts.find((part) => part.type === 'minute').value;
+    const seconds = formattedDateParts.find((part) => part.type === 'second').value;
+  
+    return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
+  };
+
+  router.post('/definition', async (req: Request, res: Response) => {
     logger.info('Workflow has been triggered..');
 
-    console.log('req body')
+    console.log('req body');
     console.log(req.body);
-    
+
     try {
         const verified = await verifySignature(req);
 
@@ -26,21 +58,71 @@ router.post('/definition', async (req: Request, res: Response) => {
             const actionDefinitionId = req.body?.origin?.actionDefinitionId;
             const objectType = req.body?.object?.objectType;
             const objectId = req.body?.object?.objectId;
-            
-            logger.info(`Processing ${source} with id ${sourceId} and action ${actionDefinitionId} targeting object ${objectType} with id ${objectId}`)
+
+            logger.info(`Processing ${source} with id ${sourceId} and action ${actionDefinitionId} targeting object ${objectType} with id ${objectId}`);
 
             const portalId = req.body?.origin?.portalId;
             const dossierNumber = req.body?.object?.properties?.dossier_number;
             const establishmentNumber = req.body?.object?.properties?.establishment_number;
 
-            console.log('portalId');
-            console.log(portalId);
-            console.log('dossierNumber');
-            console.log(dossierNumber);
-            console.log('establishmentNumber');
-            console.log(establishmentNumber);
+            if (dossierNumber && portalId) {
+                const currentUser: User | null = await usersController.getUser(portalId);
 
-            return res.status(StatusCodes.OK);
+                if (currentUser) {
+                    const companyInfoUserName = currentUser.companyInfoUserName;
+                    const companyInfoPassword = currentUser.companyInfoPassword;
+
+                    if (companyInfoUserName && companyInfoPassword) {
+                        const hubToken: HubToken | null = await authController.retrieveHubToken(currentUser.hubSpotPortalId);
+
+                        if (hubToken) {
+                            let companyData: CompanyDetail;
+
+                            if (dossierNumber && establishmentNumber) {
+                                logger.info(`Establishment number ${establishmentNumber} found, updating accordingly..`);
+
+                                companyData = await companiesController.getCompanyInfo(dossierNumber, companyInfoUserName, companyInfoPassword, establishmentNumber);
+                            } else if (establishmentNumber && !dossierNumber) {
+                                logger.info(`No dossier number found, aborting workflow...`);
+
+                                return res.status(StatusCodes.OK).json({ error: 'Workflow aborted from retrying..' });
+                            } else {
+                                logger.info(`No establishment number found, updating with dossier number ${dossierNumber}..`);
+                                
+                                companyData = await companiesController.getCompanyInfo(dossierNumber, companyInfoUserName, companyInfoPassword);
+                            }
+
+                            if (companyData) {
+                                const syncDate = new Date();
+                                const formattedDate = formatDate(syncDate);
+
+                                companyData = { ...companyData, last_sync: formattedDate };
+
+                                const properties = await formatCompanyData(companyData);
+
+                                if (properties) {
+                                    const result = await companiesController.updateCompany(hubToken, objectId, properties);
+
+                                    if (result) {
+                                        return res.status(StatusCodes.OK).json(result);
+                                    } else {
+                                        logger.error('Company has not been updated (no result)');
+                                        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: 'No company has been updated' });
+                                    }
+                                }
+                            }
+                        } else {
+                            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ status: 'error', message: 'No HubSpot user found' });
+                        }
+                    } else {
+                        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ status: 'error', message: 'No compamny info username or company info password found' });
+                    }
+                } else {
+                    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ status: 'error', message: 'No user found' });
+                }
+            } else {
+                res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ status: 'error', message: 'No dossier number or portal id found' });
+            }
         } else {
             return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: 'Signature has not been verified' });
         }
@@ -48,6 +130,7 @@ router.post('/definition', async (req: Request, res: Response) => {
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ status: 'error', message: error.message });
     }
 });
+
 
 router.get('', async (req: Request, res: Response) => {
     try {
